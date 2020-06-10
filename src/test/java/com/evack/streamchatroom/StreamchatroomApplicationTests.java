c564f699541609360f53379b3d0ef13c1169adc3
Evack000 <505963089@qq.com>
2020-06-10 22:34:51 +0800

    Initial commit

---
+++ src/test/java/com/evack/streamchatroom/StreamchatroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evack.streamchatroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamchatroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
